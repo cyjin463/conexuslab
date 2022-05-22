9e73253ba651d26d9a150286a7e6a23f26bb8bb2
cyjin463 <cyjin463@hanmail.net>
2022-05-23 03:53:31 +0900

    Feat : 입력값 저장
    3개의 숫자를 입력

--- conexuslab/src/App.js
+++ conexuslab/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const App = () => {
   const [num, setNum] = React.useState([]);
+  const [checkNum, setCheckNum] = React.useState([]);
 
   // 난수 생성
   const Start = () => {
@@ -16,8 +17,20 @@ const App = () => {
     console.log(num2); // 콘솔에 정답
   };
 
+  //입력값 저장
+  const InputNum = (e) => {
+    const check = e.target.value;
+    if (check.length === 3) {
+      const firstCheck = +check.slice(0, 1);
+      const middleCheck = +check.slice(1, 2);
+      const lastCheck = +check.slice(2);
+      setCheckNum([firstCheck, middleCheck, lastCheck]);
+    }
+  };
+
   return (
     <div>
+      <input type='text' placeholder='답입력' maxLength={3} onChange={(e) => InputNum(e)}></input>
       <button onClick={() => Start()}>시작하기</button>
     </div>
   );
