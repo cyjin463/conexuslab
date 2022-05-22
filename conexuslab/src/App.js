61e0c892488d08bc89f9d95fd934a966207b3b7c
cyjin463 <cyjin463@hanmail.net>
2022-05-23 04:18:24 +0900

    Reafator : State값 초기화
    게임시작시 스트라이크, 볼, 입력값 초기화

--- conexuslab/src/App.js
+++ conexuslab/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const App = () => {
-  const [num, setNum] = React.useState([]);
-  const [checkNum, setCheckNum] = React.useState([]);
+  const [gameView, setGameView] = React.useState(false);
+  const [start, setStart] = React.useState(false);
   const [strike, setStrike] = React.useState(0);
   const [ball, setBall] = React.useState(0);
+  const [numRule, setNumRule] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [num, setNum] = React.useState([]);
+  const [checkNum, setCheckNum] = React.useState([]);
 
-  // 난수 생성
   const Start = () => {
     let num2 = [];
     for (let i = 0; i < 3; i++) {
@@ -16,12 +19,18 @@ const App = () => {
       } else i--;
     }
     setNum(num2);
-    console.log(num2); // 콘솔에 정답
+    setGameView(true);
+    setStrike(0);
+    setBall(0);
+    setCheckNum([]);
+    console.log(num2);
   };
 
-  //입력값 저장
   const InputNum = (e) => {
+    const rule = /^[1-9]{3}$/;
     const check = e.target.value;
+    setText(e.target.value);
+    rule.test(check) ? setNumRule(true) : setNumRule(false);
     if (check.length === 3) {
       const firstCheck = +check.slice(0, 1);
       const middleCheck = +check.slice(1, 2);
@@ -30,23 +39,26 @@ const App = () => {
     }
   };
 
-  //스트라이크 확인
   const StrikeCheck = () => {
     let strikecheck = 0;
-    if (num[0] === checkNum[0]) {
-      strikecheck += 1;
-    }
-    if (num[1] === checkNum[1]) {
-      strikecheck += 1;
-    }
-    if (num[2] === checkNum[2]) {
-      strikecheck += 1;
+    if (numRule === true) {
+      if (num[0] === checkNum[0]) {
+        strikecheck += 1;
+      }
+      if (num[1] === checkNum[1]) {
+        strikecheck += 1;
+      }
+      if (num[2] === checkNum[2]) {
+        strikecheck += 1;
+      }
+    } else if (numRule !== true) {
+      window.alert("0이아닌 숫자3자리 모두 입력해주세요.");
     }
     setStrike(strikecheck);
+    setStart(true);
     BallCheck();
   };
 
-  //볼 확인
   const BallCheck = () => {
     let ballcheck = 0;
     num.map((x) => {
@@ -57,11 +69,12 @@ const App = () => {
     setBall(ballcheck);
   };
 
-  //종료
   const End = () => {
     if (window.confirm("홈런!!! 게임을 다시 시작하시겠습니까?")) {
       setNum([]);
+      setText("");
       Start();
+      setStart(false);
     } else {
       window.location.reload("./App.js");
     }
@@ -69,13 +82,32 @@ const App = () => {
 
   return (
     <div>
-      <input type='text' placeholder='답입력' maxLength={3} onChange={(e) => InputNum(e)}></input>
-      <div>
-        {strike}스트라이크, {`${ball}` - `${strike}`}볼 입니다!
-      </div>
-      <button onClick={() => StrikeCheck()}>확인</button>
-      <button onClick={() => Start()}>시작하기</button>
-      <button onClick={() => End()}>종료하기</button>
+      {gameView ? (
+        <div>
+          <div>
+            <input
+              type='text'
+              value={text}
+              placeholder='답입력'
+              maxLength={3}
+              onChange={(e) => InputNum(e)}></input>
+            <button onClick={() => StrikeCheck()}>확인</button>
+          </div>
+          {start === false ? (
+            <div>게임을 시작합니다!</div>
+          ) : strike === 0 && ball === 0 && start === true ? (
+            <div>낫싱</div>
+          ) : strike === 3 ? (
+            End()
+          ) : (
+            <div>
+              {strike}스트라이크, {`${ball}` - `${strike}`}볼 입니다!
+            </div>
+          )}
+        </div>
+      ) : (
+        <button onClick={() => Start()}>시작하기</button>
+      )}
     </div>
   );
 };
